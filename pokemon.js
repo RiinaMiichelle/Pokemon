917f412076918d472a6e29e5dda12b273a653ebd
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2019-12-19 20:12:14 -0600

    current pokemon

--- pokemon.js
+++ pokemon.js
@@ -6,7 +6,8 @@ window.onload = function() {
 
 const fetchRandomPokemon = () => {
   let randomPokemon = Math.floor(Math.random() * 10)
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+  // fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
     .then(res => res.json())
     .then(pokemon => arrayOfPokemon = pokemon)
   }
@@ -17,11 +18,21 @@ const fetchRandomPokemon = () => {
 
   const displayPokemon = () => {
     const allPokemon = document.getElementById('all-pokemon')
+    console.log(arrayOfPokemon);
     arrayOfPokemon.map((pokemon, index) => {
+      console.log(pokemon);
       const li = document.createElement('li')
-      const text = document.createTextNode(`#${index}, Name: ${pokemon.name}:  ${pokemon.body}, by Picture: ${user.userId}`)
+      const text = document.createTextNode(`Name: ${pokemon}`)
+      // const imgElement = document.createElement('img');
+      // imgElement.src = user.picture.large;
       li.appendChild(text)
-      allPokemon.append(li)
+      // li.appendChild(imgElement)
+    //   const li = document.createElement('li')
+    // const text = document.createTextNode(`Name: ${getFullName(user)}`)
+    // const imgElement = document.createElement('img');
+    // imgElement.src = user.picture.large;
+    // li.appendChild(text)
+    // li.appendChild(imgElement)
     })
   }
 
